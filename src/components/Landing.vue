0dad3a5fdf707a48c30032d7e9bba8a0f7b015dd
James Hibben <jmhibben@gmail.com>
2017-03-20 08:05:04 -0500

    Landing page for the Assessment

---
+++ src/components/Landing.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+div(id="landing")
+  bwprogress(completed=3)
+  bwtips
+
+  bwassessment
+</template>
+
+<script>
+import Progress from './Progress'
+import Tips from './Tips'
+import Assessment from './Assessment'
+
+export default {
+  name: 'landing',
+  components: {
+    bwprogress: Progress,
+    bwtips: Tips,
+    bwassessment: Assessment
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import "../assets/less/helpers";
+
+@media all and (max-width: 800px) {
+  #landing {
+    display: block;
+    clear: both;
+  }
+}
+
+@media all and (min-width: 801px) {
+  #landing {
+    display: inline-block;
+    float: left;
+  }
+}
+
+bwprogress, bwtips {
+  display: inherit;
+}
+</style>
