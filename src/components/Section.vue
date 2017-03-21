5ed646d4b6710fba15fc245c3f0b2b4ccdb06d7d
James Hibben <jmhibben@gmail.com>
2017-03-20 21:28:14 -0500

    Split section for checkmarks into its own file

---
+++ src/components/Section.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+.section
+  template(v-for="i in 8")
+    bwcheck(v-bind:checked="isChecked(i)") Section {{i}}
+</template>
+
+<script>
+import Check from './Check'
+
+export default {
+  name: 'bwsection',
+  props: {
+    checked: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    completedSections: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    bwcheck: Check
+  },
+  methods: {
+    isChecked: function (index) {
+      return this.completedSections[index - 1]
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
