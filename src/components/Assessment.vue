1f50a2a9460fc898881a47f654380cc07a5ca2d2
James Hibben <jmhibben@gmail.com>
2017-03-20 21:46:34 -0500

    Outer part for Assessment component

--- src/components/Assessment.vue
+++ src/components/Assessment.vue
@@ -1,11 +1,21 @@
 <template lang="pug">
-
+#assessment-container
+  router-view
 </template>
 
 <script>
-  
+export default {
+  name: 'bwassessment'
+}
 </script>
 
 <style scoped lang="less">
+@import '../assets/less/colors';
 
+#assessment-view {
+  clear: both;
+  width: 95%;
+  margin: 0 auto;
+  background-color: lighten(@base, 30%);
+}
 </style>
