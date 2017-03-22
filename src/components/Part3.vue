edc7e30dd2d838c3e9b34dcd51b4f3650a8ec48e
James Hibben <jmhibben@gmail.com>
2017-03-21 21:52:29 -0500

    Base page for Part3

---
+++ src/components/Part3.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.part
+  p Some Text...{{name}}
+</template>
+
+<script>
+export default {
+  name: 'part3',
+  data () {
+    return {
+      name: 'part3'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
