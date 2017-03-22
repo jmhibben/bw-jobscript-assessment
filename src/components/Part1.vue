ee26a09939ad43f86d415394a6e098e2bb690e2f
James Hibben <jmhibben@gmail.com>
2017-03-21 21:51:40 -0500

    Demo page for checkboxes

---
+++ src/components/Part1.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+.part
+  p Example description of the current section. 
+  p The width of the page is constrained to 600px, much like the content for The Whether. The checkboxes below will re-organize themselves into 1-3 columns, according to how wide the screen is.
+
+  template(v-for="i in 24")
+    .check(v-bind:class="'check'+i")
+      input(
+        type="checkbox"
+        checked=false
+        v-bind:value="i"
+        v-bind:id="'chk' + i.toString()"
+      )
+      label(
+        v-bind:for="'chk' + i.toString()"
+      ) Check {{i}}
+
+</template>
+
+<script>
+export default {
+  name: 'part1',
+  data () {
+    return {
+      name: 'part1'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import '../assets/less/checks';
+</style>
