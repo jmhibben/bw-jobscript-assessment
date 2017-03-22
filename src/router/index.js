e96117f2a85d4411fc3308d0ff1917a987f6ff77
James Hibben <jmhibben@gmail.com>
2017-03-21 21:19:23 -0500

    Added subroutes for assessment

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Hello from '@/components/Hello'
 import Landing from '@/components/Landing'
+import AssessmentPart1 from '@/components/Part1'
+import AssessmentPart2 from '@/components/Part2'
+import AssessmentPart3 from '@/components/Part3'
 
 Vue.use(Router)
 
@@ -13,8 +16,25 @@ export default new Router({
     },
     {
       path: '/landing',
-      name: 'Assessment Landing',
-      component: Landing
+      component: Landing,
+      children: [
+        {
+          path: '',
+          alias: 'page1',
+          name: 'Assessment Page 1',
+          component: AssessmentPart1
+        },
+        {
+          path: 'page2',
+          name: 'Assessment Page 2',
+          component: AssessmentPart2
+        },
+        {
+          path: 'page3',
+          name: 'Assessment Page 3',
+          component: AssessmentPart3
+        }
+      ]
     }
   ]
 })
