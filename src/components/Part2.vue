54f45ec936705fb711d3a91bdca0a2c178214d6a
James Hibben <jmhibben@gmail.com>
2017-03-21 21:26:42 -0500

    Example file 2
    Demonstrates:
    - Radio button handling
    - Question/answer styling

---
+++ src/components/Part2.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+.part
+  p Description of section goes here.
+
+  template(v-for="i in 10")
+    .row(v-bind:class="(i % 2 === 0) ? '' : 'row-odd'")
+      .col.question Question {{i}}
+      .col.container
+        .col.answer
+          input(
+            type="radio" value="A"
+            v-bind:id="'q'+i.toString()+'a'"
+            v-bind:name="i"
+          )
+          label(v-bind:for="'q'+i.toString()+'a'") 1
+        .col.answer
+          input.col.answer(
+            type="radio" value="B"
+            v-bind:id="'q'+i.toString()+'b'"
+            v-bind:name="i"
+          )
+          label(v-bind:for="'q'+i.toString()+'b'") 2
+        .col.answer
+          input.col.answer(
+            type="radio" value="C"
+            v-bind:id="'q'+i.toString()+'c'"
+            v-bind:name="i"
+          )
+          label(v-bind:for="'q'+i.toString()+'c'") 3
+        .col.answer
+          input.col.answer(
+            type="radio" value="D"
+            v-bind:id="'q'+i.toString()+'d'"
+            v-bind:name="i"
+          )
+          label(v-bind:for="'q'+i.toString()+'d'") 4
+        .col.answer
+          input.col.answer(
+            type="radio" value="E"
+            v-bind:id="'q'+i.toString()+'e'"
+            v-bind:name="i"
+          )
+          label(v-bind:for="'q'+i.toString()+'e'") 5
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: 'part2',
+  data () {
+    return {
+      name: 'part2'
+    }
+  },
+  components: {
+    bwbutton: Button
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import '../assets/less/tables';
+</style>
